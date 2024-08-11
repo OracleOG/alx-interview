aae1912343a184e1e0b65ba7fce05582516b8d82
OracleOG <emmanuelnwafor212@gmail.com>
2024-08-11 17:27:38 +0100

    solved the pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,24 @@
-def pascal_triangle(n):
-    '''check pascals triangle'''
-    if n <= 0:
+#!/usr/bin/python3
+
+'''
+creating a function that returns pascals triangle
+'''
+
+
+def pascal_triangle(k):
+    if k <= 0:
         return []
-    
-    triangle = []
 
-    for i in range(n):
-        row = [1] * (i + 1)
-        for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(row)
+    pasc_array = []
 
-    return triangle
+    for nth_row in range(k):
+        row = []
+        for nth_col in range(nth_row + 1):
+            if nth_col == 0 or nth_col == nth_row:
+                row.append(1)
+            else:
+                row.append(pasc_array[nth_row - 1][nth_col - 1]
+                           + pasc_array[nth_row - 1][nth_col])
+        pasc_array.append(row)
+
+    return pasc_array
